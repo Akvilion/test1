e5cda77f724c78ee1fcdcae5b27e8e706f704158
Akvilion <ianko.andrii@gmail.com>
2021-05-29 11:56:58 +0300

    implement login logout

--- project1/start.py
+++ project1/start.py
@@ -1,10 +1,12 @@
+import logging
 from flask import Flask
 from flask_admin import Admin
+from flask_login.utils import login_user
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin.contrib.sqla import ModelView
 from sqlalchemy.orm import backref
 import os
-from flask_login import UserMixin
+from flask_login import UserMixin, LoginManager, current_user, login_user, logout_user
 
 
 path = os.getcwd()
@@ -17,6 +19,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+filename
 app.config['SECRET_KEY']= '123abc'
 
 db = SQLAlchemy(app)
+login = LoginManager(app)
+
+
+@login.user_loader
+def load_user(user_id):
+    return UserAdmin.query.get(user_id)
+
+
+
+
 
 class UserAdmin(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -47,15 +59,28 @@ class Books(db.Model):
 
 
 class MyModelView(ModelView):
-    pass
+    def is_accessible(self):
+        return current_user.is_authenticated
 
+
+@app.route('/login')
+def login():
+    user = UserAdmin.query.get(1)
+    login_user(user)
+    return 'Ligged in'
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return 'You have been Logged out'
 
 
 admin = Admin(app)
 
-admin.add_view(ModelView(Authors, db.session))
-admin.add_view(ModelView(Books, db.session))
-admin.add_view(ModelView(UserAdmin, db.session))
+admin.add_view(MyModelView(Authors, db.session))  # щоб заборонити показувати
+admin.add_view(MyModelView(Books, db.session))  # MyModelView щоб заборонити показувати
+admin.add_view(MyModelView(UserAdmin, db.session))
 
 
 if __name__ == '__main__':
